731faabb5d0c6c569c870228deb891533820f6db
manmaxmotivation <manmaxmotivation@gmail.com>
2025-01-08 13:03:46 +0300

    Добавлены функции для отправки ETH на несколько адресов, проверки статуса транзакций, а также улучшена обработка ошибок и логирование. Реализована поддержка нескольких RPC-серверов для повышения надежности. Внесены изменения в логику вычисления комиссий и остаточного баланса.

--- README.md
+++ README.md
@@ -33,7 +33,7 @@
 
 1. Клонируйте репозиторий:
    ```bash
-git clone https://github.com/yourusername/multi-send-eth.git
+   git clone https://github.com/yourusername/multi-send-eth.git
 
 ## Установите зависимости:
 pip install -r requirements.txt
--- multi_send_sepolia_eth.py
+++ multi_send_sepolia_eth.py
@@ -2,7 +2,15 @@ from web3 import Web3
 from dotenv import load_dotenv
 import os
 import time
+import logging
 from decimal import Decimal
+
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()]
+)
 
 # Загружаем переменные окружения из .env файла
 load_dotenv()
@@ -11,21 +19,51 @@ load_dotenv()
 private_key = os.getenv("PRIVATE_KEY")
 sender_address = os.getenv("SENDER_ADDRESS")
 
-# Подключение к RPC-серверу
-rpc_url = "https://1rpc.io/sepolia"  # Замените на ваш RPC URL
-web3 = Web3(Web3.HTTPProvider(rpc_url))
+# Список RPC URL для подключения
+rpc_urls = [
+    "https://1rpc.io/sepolia",  # Основной RPC
+    "https://ethereum-sepolia-rpc.publicnode.com",  # Альтернативный RPC 1
+    "https://endpoints.omniatech.io/v1/eth/sepolia/public",  # Альтернативный RPC 2
+    "https://eth-sepolia.public.blastapi.io"  # Альтернативный RPC 3
+]
 
-# Убедитесь, что соединение установлено
-if not web3.is_connected():
-    raise Exception("Не удалось подключиться к сети!")
+# Попытка подключения к одному из RPC
+web3 = None
+for rpc_url in rpc_urls:
+    try:
+        web3 = Web3(Web3.HTTPProvider(rpc_url))
+        if web3.is_connected():
+            logging.info(f"Успешно подключено к RPC: {rpc_url}")
+            break
+    except Exception as e:
+        logging.warning(f"Ошибка подключения к {rpc_url}: {e}")
+
+# Добавление задержки перед отправкой транзакции
+time.sleep(1)  # Задержка в 1 секунды между попытками
+
+if not web3 or not web3.is_connected():
+    raise Exception("Не удалось подключиться ни к одному из RPC серверов!")
+
+# Функция для получения текущей цены газа с обработкой ошибки 429
+def get_gas_price():
+    gas_price = None
+    while not gas_price:
+        try:
+            gas_price = web3.eth.gas_price  # текущая цена газа
+        except Exception as e:
+            logging.warning(f"Ошибка при получении цены газа: {e}. Попробуем снова через 10 секунд...")
+            time.sleep(1)  # Задержка перед повторной попыткой
+    return gas_price
 
 # Функция для отправки ETH
 def send_eth(recipient_address, amount_in_eth):
+    logging.info(f"Начинаем отправку {amount_in_eth} ETH на адрес {recipient_address}")
+
     # Получаем nonce для отправителя
     nonce = web3.eth.get_transaction_count(sender_address)
 
     # Получаем текущую цену газа
-    gas_price = web3.eth.gas_price  # текущая цена газа
+    gas_price = get_gas_price()
 
     # Увеличиваем цену газа в 1.05 раза для повышения вероятности успешной транзакции
     gas_price = int(gas_price * 1.05)
@@ -36,7 +74,6 @@ def send_eth(recipient_address, amount_in_eth):
         'value': web3.to_wei(amount_in_eth, 'ether'),
         'gas': int(
             web3.eth.estimate_gas({'to': recipient_address, 'value': web3.to_wei(amount_in_eth, 'ether')}) * 1.05),
-        # Преобразуем в целое число
         'gasPrice': gas_price,
         'nonce': nonce,
         'chainId': 11155111  # Для сети Sepolia
@@ -48,31 +85,31 @@ def send_eth(recipient_address, amount_in_eth):
     # Отправка транзакции
     try:
         tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
-        print(f"Транзакция отправлена с хэшем: {web3.to_hex(tx_hash)}")
+        logging.info(f"Транзакция отправлена с хэшем: {web3.to_hex(tx_hash)}")
 
-        # Задержка в 1 секунду между транзакциями
-        time.sleep(1)
 
         # Возвращаем хэш транзакции для последующей проверки
         return tx_hash
 
     except Exception as e:
-        print(f"Ошибка отправки транзакции: {e}")
+        logging.error(f"Ошибка отправки транзакции: {e}")
         return None
 
 # Функция для проверки статуса транзакции
 def check_transaction_status(tx_hash):
+    logging.info(f"Ожидаем подтверждения транзакции с хэшем {web3.to_hex(tx_hash)}")
+
     try:
         # Получаем квитанцию о транзакции
         receipt = web3.eth.wait_for_transaction_receipt(tx_hash, timeout=120)
 
         # Проверяем статус транзакции
         if receipt['status'] == 1:
-            print(f"Транзакция с хэшем {web3.to_hex(tx_hash)} была успешно подтверждена.")
+            logging.info(f"Транзакция с хэшем {web3.to_hex(tx_hash)} была успешно подтверждена.")
         else:
-            print(f"Транзакция с хэшем {web3.to_hex(tx_hash)} не прошла.")
+            logging.warning(f"Транзакция с хэшем {web3.to_hex(tx_hash)} не прошла.")
     except Exception as e:
-        print(f"Ошибка при проверке транзакции: {e}")
+        logging.error(f"Ошибка при проверке транзакции: {e}")
 
 # Функция для проверки валидности Ethereum адреса
 def is_valid_ethereum_address(address):
@@ -88,9 +125,9 @@ def load_addresses(file_path):
                 if is_valid_ethereum_address(address):
                     addresses.append(Web3.to_checksum_address(address))  # Преобразуем в checksum-формат
                 else:
-                    print(f"Некорректный адрес: {address}")
+                    logging.warning(f"Некорректный адрес: {address}")
             else:
-                print(f"Пустая строка в файле: {line}")
+                logging.warning(f"Пустая строка в файле: {line}")
     return addresses
 
 # Путь к файлу с адресами получателей
@@ -135,28 +172,30 @@ expected_balance = Decimal(current_balance) - Decimal(total_amount) - total_gas_
 formatted_expected_balance = "{:,.6f}".format(expected_balance)
 
 # Выводим информацию перед запуском
-print(f"Информация перед запуском:")
-print(f"Отправитель: {sender_address}")
-print(f"Сумма на отправку: {formatted_total_amount} ETH (по {formatted_amount_in_eth} ETH на адрес)")
-print(f"Баланс отправителя: {formatted_current_balance} ETH")
-print(f"Количество получателей: {len(addresses)}")
-print(f"Ожидаемый остаток после отправки: {formatted_expected_balance} ETH")
-print(f"Ожидаемая комиссия за транзакции: {formatted_gas_fee} ETH")
+logging.info(f"Информация перед запуском:")
+logging.info(f"Отправитель: {sender_address}")
+logging.info(f"Сумма на отправку: {formatted_total_amount} ETH (по {formatted_amount_in_eth} ETH на адрес)")
+logging.info(f"Баланс отправителя: {formatted_current_balance} ETH")
+logging.info(f"Количество получателей: {len(addresses)}")
+logging.info(f"Ожидаемый остаток после отправки: {formatted_expected_balance} ETH")
+logging.info(f"Ожидаемая комиссия за транзакции: {formatted_gas_fee} ETH")
 
 # Подтверждаем перед запуском
 confirmation = input("Если информация верна, введите 'OK' для продолжения: ")
 
 if confirmation.lower() == "ok":
-    print("Запуск транзакций...")
+    logging.info("Запуск транзакций...")
+
     # Список для хранения хэшей транзакций
     transaction_hashes = []
 
     # Отправка транзакций всем получателям с проверкой статуса
+    start_time = time.time()  # Запускаем таймер
     for address in addresses:
         # Отправляем транзакцию
         tx_hash = send_eth(address, amount_in_eth)
         if tx_hash:
-            print(f"Транзакция отправлена с хэшем: {web3.to_hex(tx_hash)}")
+            logging.info(f"Транзакция отправлена с хэшем: {web3.to_hex(tx_hash)}")
 
             # Проверяем статус отправленной транзакции
             check_transaction_status(tx_hash)
@@ -164,7 +203,7 @@ if confirmation.lower() == "ok":
             # Добавляем хэш транзакции в список
             transaction_hashes.append(tx_hash)
         else:
-            print(f"Не удалось отправить транзакцию на {address}")
+            logging.error(f"Не удалось отправить транзакцию на {address}")
 
     # Записываем хэши в файл для дальнейшей проверки
     with open('tx_hashes.txt', 'w') as file:
@@ -180,10 +219,13 @@ if confirmation.lower() == "ok":
     total_eth_sent = total_transactions * amount_in_eth
     total_addresses = len(addresses)
 
-    print(f"\nИтоговый отчет:")
-    print(f"Количество отправленных транзакций: {total_transactions}")
-    print(f"Общее количество отправленного ETH: {total_eth_sent:.6f}")
-    print(f"Количество адресов в списке: {total_addresses}")
-    print(f"Остаток на балансе после завершения: {formatted_final_balance} ETH")
+    elapsed_time = time.time() - start_time  # Время выполнения
+
+    logging.info(f"\nИтоговый отчет:")
+    logging.info(f"Количество отправленных транзакций: {total_transactions}")
+    logging.info(f"Общее количество отправленного ETH: {total_eth_sent:.6f}")
+    logging.info(f"Количество адресов в списке: {total_addresses}")
+    logging.info(f"Остаток на балансе после завершения: {formatted_final_balance} ETH")
+    logging.info(f"Время выполнения: {elapsed_time:.2f} секунд")
 else:
-    print("Запуск отменен.")
+    logging.info("Запуск отменен.")
